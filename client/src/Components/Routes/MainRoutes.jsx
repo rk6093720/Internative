a782dafbb24aafe5b2c493ff95456d60675f5bb8
Anup Haldar <haldaranup746@gmail.com>
2023-01-14 14:34:37 +0530

    add home

--- client/src/Components/Routes/MainRoutes.jsx
+++ client/src/Components/Routes/MainRoutes.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Signup from '../Login/Signup'
 import Login from '../Login/Login'
+import Home from '../Home/Home'
 
 const MainRoutes = () => {
   return (
     <>
         <Routes>
+            <Route path='/' element={<Home/>} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/login' element={<Login />} />
         </Routes>
